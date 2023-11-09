e24a6638019b1a0173b9c47011a0e5dd60db987b
s13g <ayflix0@gmail.com>
2023-11-09 10:55:19 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/chirp/finder/chirp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chirp.finder.chirp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:chirp/screens/start/start_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+
+final theme = ThemeData(
+  colorScheme: kColorScheme,
+  useMaterial3: true,
+  // Customizing text styles
+  textTheme: GoogleFonts.interTextTheme(),
+  appBarTheme: const AppBarTheme().copyWith(
+    backgroundColor: kColorScheme.onPrimaryContainer,
+    foregroundColor: kColorScheme.primaryContainer,
+  ),
+  cardTheme: const CardTheme().copyWith(
+    color: kColorScheme.secondaryContainer,
+    margin: const EdgeInsets.symmetric(
+      horizontal: 16,
+      vertical: 8,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: kColorScheme.primaryContainer,
+    ),
+  ),
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chirp',
+      theme: theme,
+      themeMode: ThemeMode.system,
+      home: const StartScreen(),
+    );
+  }
+}
